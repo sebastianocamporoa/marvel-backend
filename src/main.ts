4bb13b024adbad0f6b4a81d4b61bbca3180c611c
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-22 18:45:40 -0500

    se agrega configuracion de cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://marvel-frontend-production.up.railway.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
