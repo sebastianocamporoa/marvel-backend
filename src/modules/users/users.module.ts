0e818289221b91f08b4fcfe8eec5ba36cfb619f5
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-22 14:49:14 -0500

    se corrigen importaciones

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './users.service';
-import { UserController } from './users.controller';
+import { UsersController } from './users.controller';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserRepository],
-  controllers: [UserController],
+  controllers: [UsersController],
   exports: [UserService]
 })
 export class UserModule {}
