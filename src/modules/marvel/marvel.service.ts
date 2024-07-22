fff73e728028c0bce94d6f2c90f9cf5a5d8010c7
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-07-22 14:12:04 -0500

    se agregan keys de marvel

--- src/modules/marvel/marvel.service.ts
+++ src/modules/marvel/marvel.service.ts
@@ -4,16 +4,25 @@ import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class MarvelService {
   private readonly apiUrl = 'https://gateway.marvel.com/v1/public/comics';
-  private readonly apiKey = 'your_marvel_api_key';
+  private readonly publicKey = '60cfb37eee49438efb47affaf4866fd6';
+  private readonly privateKey = 'c9be6c673fe295924d9ec91755bc82f6ba0127eb';
 
   constructor(private httpService: HttpService) {}
 
+  private getAuthParams(): string {
+    const timestamp = new Date().getTime();
+    const hash = CryptoJS.MD5(timestamp + this.privateKey + this.publicKey).toString();
+    return `ts=${timestamp}&apikey=${this.publicKey}&hash=${hash}`;
+  }
+
   getComics(): Observable<AxiosResponse<any>> {
-    return this.httpService.get(`${this.apiUrl}?apikey=${this.apiKey}`).pipe(
+    const authParams = this.getAuthParams();
+    return this.httpService.get(`${this.apiUrl}?${authParams}`).pipe(
       map(response => response.data)
     );
   }
