fdd6cc24c20c3e7a158303cffb7915642e6eb2e8
Sebastian Ocampo <sebastian45roa@gmail.com>
2024-08-02 12:56:10 -0500

    se permiten todos los origenes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://marvel-frontend-production.up.railway.app',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
